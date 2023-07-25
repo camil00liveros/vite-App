207dc86daa31de10ffcffd46804ab084dfaa4827
camil00liveros <105334389+camil00liveros@users.noreply.github.com>
2023-07-25 09:14:54 -0500

    base update

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -5,7 +5,7 @@ function TaskCard({task}) {
   const {deleteTask} = useContext(TaskContext)
 
   return (
-    <div className="bg-gray-500 text-white p-4 rounded-md">
+    <div className="bg-gray-400 text-white p-4 rounded-md">
       <h1 className="text-xl font-bold capitalize">{task.title}</h1>
       <p className="text-gray-900 text-sm">{task.description}</p>
       <button className="bg-red-500 py-1 px-2 rounded-md mt4 hover:bg-red-300" onClick={()=> deleteTask(task.id)}>
